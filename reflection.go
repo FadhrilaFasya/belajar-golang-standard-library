54cf6d88bc32e892b25aa9b1ec10257e99678c44
fadhrila_fasya <fadhrilafasya@gmail.com>
2025-10-09 14:51:39 +0700

    VALIDATION LIBRARY

--- reflection.go
+++ reflection.go
@@ -6,27 +6,57 @@ import (
 )
 
 type Sample struct {
-	Name string `required: true max: "10"`
+	Name string `required:"true" max:"10"`
 }
 
 type Person struct {
-	Name     string `required: true max: "10"`
-	Address  string `required: true max: "100"`
-	Email    string `required: true max: "100"`
+	Name    string `required:"true" max:"10"`
+	Address string `required:"true" max:"10"`
+	Email   string `required:"true" max:"10"`
 }
 
-// readField function to read struct fields using reflection
-func readField(value any) { 
-	valueType := reflect.TypeOf(value) // get the type of the value
+// readField membaca tag pada struct
+func readField(value any) {
+	var valueType reflect.Type = reflect.TypeOf(value)
 	fmt.Println("Type Name", valueType.Name())
-	for i := 0; i < valueType.NumField(); i++ { // iterate over the fields
-		structField := valueType.Field(i) // get the field at index i
-		fmt.Println(structField, "with type", structField.Type)
+
+	// membaca field pada struct
+	for i := 0; i < valueType.NumField(); i++ {
+		var structField reflect.StructField = valueType.Field(i)
+		fmt.Println(structField.Name, "with type", structField.Type)
 		fmt.Println(structField.Tag.Get("required"))
 		fmt.Println(structField.Tag.Get("max"))
 	}
 }
+
+// IsValid mengecek apakah field yang required sudah diisi
+func IsValid(value any) (result bool) {
+	result = true
+	t := reflect.TypeOf(value)
+
+	// membaca field pada struct
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if f.Tag.Get("required") == "true" {
+			data := reflect.ValueOf(value).Field(i).Interface() // ambil data field
+			// cek data field
+			result = data != ""
+			if result == false {
+				return result
+			}
+		}
+	}
+	return result
+}
+
 func main() {
-	readField(Sample{Name: "Eko"})
-	readField(Person{Name: "Budi", Address: "Indonesia", Email: "budi@example.com"})
+	readField(Sample{"Eko"})
+	readField(Person{"Eko", "", ""})
+
+	person := Person{
+		Name:    "ada",
+		Address: "ada",
+		Email:   "",
+	}
+	fmt.Println(IsValid(person))
 }
